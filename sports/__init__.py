2f5c5766c9f7eb17083780b751d18882b7ca8847
ChristianLoza <Christianlzap@gmail.com>
2023-01-29 18:08:10 -0500

    update: database init app

--- sports/__init__.py
+++ sports/__init__.py
@@ -7,17 +7,16 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    
     environment_configuration = os.environ['CONFIGURATION_SETUP']
-    
     app.config.from_object(environment_configuration)
-    
     db.init_app(app)
     
     with app.app_context():
         #registro del blueprint
         from .sports_api import sports_api_blueprint
         app.register_blueprint(sports_api_blueprint)
+        db.init_app(app)
+        db.create_all()
         return app
     
     
